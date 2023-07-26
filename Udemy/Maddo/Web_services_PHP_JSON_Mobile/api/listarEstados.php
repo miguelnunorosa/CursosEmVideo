09d2f8008c3c263e2753abd63444beaf5a3a7a18
Miguel Rosa <miguelrosa.programacao@gmail.com>
2023-07-26 15:15:23 +0100

    course: Web Services - Listar Estados.

--- Udemy/Maddo/Web_services_PHP_JSON_Mobile/api/listarEstados.php
+++ Udemy/Maddo/Web_services_PHP_JSON_Mobile/api/listarEstados.php
@@ -1,32 +1,40 @@
 <?php
     global $conection;
-    include ('dbConnection.php');
-
-
 
     const TABLE = "estado";
     const SQL_READ = 'SELECT * FROM ' . TABLE ;
+    const API_TOKEN = "api_token_example";
 
 
-    $query = mysqli_prepare($conection, SQL_READ);
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        include ('dbConnection.php');
 
-    mysqli_stmt_execute($query);
-    mysqli_stmt_store_result($query);
-    mysqli_stmt_bind_result($query, $id, $sigla, $nome);
+        $get_api_token = "api_token_example";
 
-    $data = array();
+        if($get_api_token == API_TOKEN){
+            $query = mysqli_prepare($conection, SQL_READ);
 
-    if(mysqli_stmt_num_rows($query) > 0){
-        while (mysqli_stmt_fetch($query)){
-            array_push($data, array("id"=>$id, "sigla"=>$sigla, "nome"=>$nome));
+            mysqli_stmt_execute($query);
+            mysqli_stmt_store_result($query);
+            mysqli_stmt_bind_result($query, $id, $sigla, $nome);
+
+            $data = array();
+
+            if(mysqli_stmt_num_rows($query) > 0){
+                while (mysqli_stmt_fetch($query)){
+                    array_push($data, array("id"=>$id, "sigla"=>$sigla, "nome"=>$nome));
+                }
+
+                echo json_encode($data);
+            }else{
+                echo "Error. No data available.";
+            }
+
+        }else{
+            $response['auth_key'] = false;
+            echo "Error. Authentication refused.";
         }
 
-        echo json_encode($data);
-    }else{
-        echo json_encode($data);
     }
-
-
-
 
 ?>
