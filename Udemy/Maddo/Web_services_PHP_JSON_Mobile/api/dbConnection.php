35b8575af48ab38c2a394ba59b58b8235fb94488
Miguel Rosa <miguelrosa.programacao@gmail.com>
2023-07-26 15:25:32 +0100

    course: Web Services - update dbConnection

--- Udemy/Maddo/Web_services_PHP_JSON_Mobile/api/dbConnection.php
+++ Udemy/Maddo/Web_services_PHP_JSON_Mobile/api/dbConnection.php
@@ -7,6 +7,7 @@
     define('PASS', 'mertola21');
     define('DB', 'db_curso_udemy');
     define('CHARSET', 'utf8');
+    define('API_TOKEN', 'api_token_example');
 
 
     $conection = mysqli_connect(HOST, USER, PASS, DB) or die("Falha!");
