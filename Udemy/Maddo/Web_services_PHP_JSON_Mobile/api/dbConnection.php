52d94d25b43ce4de9458808ff7e2dd09e8064cc4
Miguel Rosa <miguelrosa.programacao@gmail.com>
2023-07-26 14:29:28 +0100

    course: Web Services - Listar Estados

--- Udemy/Maddo/Web_services_PHP_JSON_Mobile/api/dbConnection.php
+++ Udemy/Maddo/Web_services_PHP_JSON_Mobile/api/dbConnection.php
@@ -6,9 +6,8 @@
     define('DB', 'db_curso_udemy');
     define('CHARSET', 'utf8');
 
-    
-    $conn = mysqli_connect(HOST, USER, PASS, DB) or die("Falha!");
-    
-    mysqli_set_charset($conn, CHARSET);
 
+    $conection = mysqli_connect(HOST, USER, PASS, DB) or die("Falha!");
+
+    mysqli_set_charset($conection, CHARSET);
 ?>
--- Udemy/Maddo/Web_services_PHP_JSON_Mobile/api/listarEstados.php
+++ Udemy/Maddo/Web_services_PHP_JSON_Mobile/api/listarEstados.php
@@ -1,12 +1,14 @@
 <?php
-    include('DBConnection.php');
+global $conection;
+include ('dbConnection.php');
+
 
 
     const TABLE = "estado";
     const SQL_READ = 'SELECT * FROM ' . TABLE ;
 
 
-    $query = mysqli_prepare($conn, SQL_READ);
+    $query = mysqli_prepare($conection, SQL_READ);
 
     mysqli_stmt_execute($query);
     mysqli_stmt_store_result($query);
