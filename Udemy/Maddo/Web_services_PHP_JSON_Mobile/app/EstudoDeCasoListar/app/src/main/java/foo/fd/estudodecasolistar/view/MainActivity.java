26b354e9a858827aafd1db99f6968f726f286252
Miguel Rosa <miguelrosa.programacao@gmail.com>
2023-07-28 13:02:56 +0100

    Course: Web Services - CRUD: Delete complete

--- Udemy/Maddo/Web_services_PHP_JSON_Mobile/app/EstudoDeCasoListar/app/src/main/java/foo/fd/estudodecasolistar/view/MainActivity.java
+++ Udemy/Maddo/Web_services_PHP_JSON_Mobile/app/EstudoDeCasoListar/app/src/main/java/foo/fd/estudodecasolistar/view/MainActivity.java
@@ -10,6 +10,10 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -32,6 +36,14 @@ import foo.fd.estudodecasolistar.R;
 
 public class MainActivity extends AppCompatActivity {
     Settings settings = new Settings();
+    Button btnListarEstados;
+    Button btnListarCidades;
+    Button btnDeletarCidade;
+
+    EditText editIdCidade;
+    TextView txtResultado;
+
+    int idCidade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,13 +53,46 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         String token = settings.APITOKEN;
+        createFields();
 
-        ListarEstadosAsyncTask task = new ListarEstadosAsyncTask(token);
-        task.execute(); //start async process
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
+        btnListarEstados.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ListarEstadosAsyncTask task = new ListarEstadosAsyncTask(token);
+                task.execute(); //start async process
+            }
+        });
+
+        //TODO
+        btnListarCidades.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ListarCidadesAsyncTask task = new ListarCidadesAsyncTask(token);
+                task.execute();
+            }
+        });
+
+        //TODO
+        btnDeletarCidade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                idCidade = Integer.parseInt(editIdCidade.getText().toString());
+
+                DeleteCidadesAsyncTask task = new DeleteCidadesAsyncTask(token, idCidade);
+                task.execute();
+            }
+        });
+
+
+
+    }
+    private void createFields(){
+        btnListarEstados = findViewById(R.id.btnListarEstados);
+        btnListarCidades = findViewById(R.id.btnListarCidades);
+        btnDeletarCidade = findViewById(R.id.btnDeletarCidade);
+        editIdCidade = findViewById(R.id.editIdCidade);
+        txtResultado = findViewById(R.id.txtResultado);
     }
 
     @Override
@@ -71,7 +116,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 
 
     public class ListarEstadosAsyncTask extends AsyncTask<String, String, String> {
@@ -197,5 +241,250 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    public class ListarCidadesAsyncTask extends AsyncTask<String, String, String> {
+
+        String api_token, query;
+        HttpURLConnection urlConnection;
+        URL url = null;
+        Uri.Builder builder;
+        int responseCode;
+
+
+
+        public ListarCidadesAsyncTask(String token) {
+            this.api_token = token;
+
+            this.builder = new Uri.Builder();
+            builder.appendQueryParameter("api_token", api_token);
+        }
+
+        @Override
+        protected void onPreExecute(){
+            Log.i("API-Listar", "OnPreExecute() pass");
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Log.i("API-Listar", "doInBackground() pass");
+
+            //create content for URL
+            try{
+                url = new URL(settings.APIURL + settings.API_LISTARCIDADE);
+            }catch (MalformedURLException e){
+                Log.i("API-Listar", "doInBackground() => " + e.getMessage());
+            }
+
+            //create HTTP Request (Post) and the result will be an arrayJson
+            try {
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(settings.READ_TIMEOUT);
+                urlConnection.setConnectTimeout(settings.CONNECTION_TIMEOUT);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setRequestProperty("charset", settings.CHARSET);
+                urlConnection.setDoInput(true);
+                urlConnection.setDoOutput(true);
+
+                urlConnection.connect();
+            }catch (Exception e){
+                Log.i("API-Listar", "doInBackground() => " + e.getMessage());
+            }
+
+
+            //add token or another params
+            try{
+                query = builder.build().getEncodedQuery();
+
+                OutputStream stream = urlConnection.getOutputStream();
+
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stream, settings.CHARSET));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+
+                stream.close();
+
+                urlConnection.connect();
+            }catch (Exception e){
+                Log.i("API-Listar", "doInBackground() => " + e.getMessage());
+            }
+
+            //get response (JSON format) and response code (200 / 404 / 503)
+            try {
+                responseCode = urlConnection.getResponseCode();
+
+                if(responseCode == HttpURLConnection.HTTP_OK){
+                    InputStream input = urlConnection.getInputStream();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while( (line = reader.readLine()) != null){
+                        result.append(line);
+                    }
+
+                    return result.toString();
+                }else{
+                    //return http error
+                    return "HTTP ERROR: " + responseCode;
+                }
+            }catch (Exception e){
+                Log.i("API-Listar", "doInBackground() => " + e.getMessage());
+            }finally {
+                urlConnection.disconnect();
+            }
+
+            return "Process complete!";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.i("API-Listar", "onPostExecute() result: " + result);
+            //super.onPostExecute(result);
+
+            try{
+                Cidade cidade;
+                JSONArray jsonArray = new JSONArray(result);
+
+                if (jsonArray.length() != 0){
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        cidade = new Cidade(jsonObject.getInt("id"),
+                                jsonObject.getInt("estadoID"),
+                                jsonObject.getString("nome"));
+
+                        Log.i("API-Listar: ", "Cidade " + cidade.getId() + ": " + cidade.getEstadoID() + " - " + cidade.getNome() );
+                    }
+                }
+
+            }catch (Exception e){
+                Log.i("API-Listar", "onPostExecute() => " + e.getMessage());
+            }
+        }
+
+    }
+
+
+    public class DeleteCidadesAsyncTask extends AsyncTask<String, String, String>{
+
+        String api_token, query;
+        HttpURLConnection urlConnection;
+        URL url = null;
+        Uri.Builder builder;
+        int responseCode;
+
+
+        public DeleteCidadesAsyncTask(String token, int idCidade) {
+            this.api_token = token;
+
+            this.builder = new Uri.Builder();
+            builder.appendQueryParameter("api_token", api_token);
+            builder.appendQueryParameter("idCidade", String.valueOf(idCidade));
+        }
+
+        @Override
+        protected void onPreExecute(){
+            Log.i("API-Listar", "OnPreExecute() pass");
+        }
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Log.i("API-Listar", "doInBackground() pass");
+
+            //create content for URL
+            try{
+                url = new URL(settings.APIURL + settings.API_LISTARCIDADE);
+            }catch (MalformedURLException e){
+                Log.i("API-Listar", "doInBackground() => " + e.getMessage());
+            }
+
+            //create HTTP Request (Post) and the result will be an arrayJson
+            try {
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(settings.READ_TIMEOUT);
+                urlConnection.setConnectTimeout(settings.CONNECTION_TIMEOUT);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setRequestProperty("charset", settings.CHARSET);
+                urlConnection.setDoInput(true);
+                urlConnection.setDoOutput(true);
+
+                urlConnection.connect();
+            }catch (Exception e){
+                Log.i("API-Listar", "doInBackground() => " + e.getMessage());
+            }
+
+
+            //add token or another params
+            try{
+                query = builder.build().getEncodedQuery();
+
+                OutputStream stream = urlConnection.getOutputStream();
+
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stream, settings.CHARSET));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+
+                stream.close();
+
+                urlConnection.connect();
+            }catch (Exception e){
+                Log.i("API-Listar", "doInBackground() => " + e.getMessage());
+            }
+
+            //get response (JSON format) and response code (200 / 404 / 503)
+            try {
+                responseCode = urlConnection.getResponseCode();
+
+                if(responseCode == HttpURLConnection.HTTP_OK){
+                    InputStream input = urlConnection.getInputStream();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while( (line = reader.readLine()) != null){
+                        result.append(line);
+                    }
+
+                    return result.toString();
+                }else{
+                    //return http error
+                    return "HTTP ERROR: " + responseCode;
+                }
+            }catch (Exception e){
+                Log.i("API-Listar", "doInBackground() => " + e.getMessage());
+            }finally {
+                urlConnection.disconnect();
+            }
+
+            return "Process complete!";
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.i("API-Listar", "onPostExecute() result: " + result);
+            //super.onPostExecute(result);
+
+            try{
+                JSONObject jsonObject = new JSONObject(result);
+
+                if(jsonObject.getBoolean("deleted")){
+                    txtResultado.setText("Deleted record: " + idCidade);
+                    Log.i("API-Listar", "onPostExecute() Delete OK");
+                }else{
+                    txtResultado.setText("Error deleting " + idCidade);
+                    Log.i("API-Listar", "onPostExecute() Deleting Error: " + jsonObject.getString("SQL"));
+                }
+
+            }catch (Exception e){
+                Log.i("API-Listar", "onPostExecute() => " + e.getMessage());
+            }
+        }
+
+    }
 
 }
