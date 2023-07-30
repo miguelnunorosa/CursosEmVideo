bb02bffadb5ca2f2c0dce80423e6f1bf764897d8
Miguel Rosa <miguelrosa.programacao@gmail.com>
2023-07-30 18:26:37 +0100

    FormacaoDeveloper: Database Seccao1 - add class

---
+++ Udemy/Maddo/FormacaoDeveloper_BaseDados_SQL_NoQSL/app/FDDatabase/app/src/main/java/io/github/miguelnunorosa/fddatabase/controller/AlunoController.java
@@ -0,0 +1,28 @@
+package io.github.miguelnunorosa.fddatabase.controller;
+
+public class AlunoController {
+
+    public AlunoController() {}
+
+
+    //generic methods for CRUD (Create/Read/Update/Delete)
+
+    public void save(){}
+
+
+    public void delete(){}
+
+
+    public void update(){}
+
+
+    public void read(){}
+
+
+    public void filter(){}
+
+
+    public void changeStatus(){}
+
+
+}
