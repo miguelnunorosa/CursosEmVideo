012b2fd55dd160518cabecb1d4dccd1ceaa67827
Miguel Rosa <miguelrosa.programacao@gmail.com>
2023-08-03 18:00:52 +0100

    DesenvolvimentoAndroid: AppListaVip - Using Controller

--- Udemy/Maddo/Desenvolvimento_Android_2023_13_apps_completas/AppListaVip/app/src/main/java/io/github/miguelnunorosa/applistavip/controller/PessoaController.java
+++ Udemy/Maddo/Desenvolvimento_Android_2023_13_apps_completas/AppListaVip/app/src/main/java/io/github/miguelnunorosa/applistavip/controller/PessoaController.java
@@ -1,4 +1,28 @@
 package io.github.miguelnunorosa.applistavip.controller;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import io.github.miguelnunorosa.applistavip.model.Pessoa;
+
 public class PessoaController {
+
+    public void saveData(Pessoa pessoa) {
+        Log.d("AppListaVIP", "Saved " + pessoa.toString());
+    }
+
+
+
+
+
+
+
+    @NonNull
+    @Override
+    public String toString() {
+        Log.d("AppListaVIP", "Controller Pessoa started");
+        return super.toString();
+    }
+
 }
