d873beae805def5e53fe2245d60b7ebf7cd80f21
Miguel Rosa <miguelrosa.programacao@gmail.com>
2023-08-06 16:20:47 +0100

    DesenvolvimentoAndroid: AppListaVip - complete

--- Udemy/Maddo/Desenvolvimento_Android_2023_13_apps_completas/AppListaVip/app/src/main/java/io/github/miguelnunorosa/applistavip/view/SplashActivity.java
+++ Udemy/Maddo/Desenvolvimento_Android_2023_13_apps_completas/AppListaVip/app/src/main/java/io/github/miguelnunorosa/applistavip/view/SplashActivity.java
@@ -2,15 +2,40 @@ package io.github.miguelnunorosa.applistavip.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import io.github.miguelnunorosa.applistavip.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static final int TIMEOUT_SPLASH = 3000; //time in ms
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        changeSplashScreen();
     }
+
+    private void changeSplashScreen() {
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() { //process in TIMEOUT time
+                Intent mainActivityScreen = new Intent(SplashActivity.this, MainActivity.class);
+
+                //Note:
+                //Intent mainActivityScreen = new Intent(from screen, to screen);
+
+                startActivity(mainActivityScreen); //open Screen
+                finish(); //don't allow to go back (previous screen)
+            }
+        }, TIMEOUT_SPLASH);
+
+    }
+
+
 }
