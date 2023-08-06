8f3dcdead257466f2425ddf0ac984a5f2ca2433a
Miguel Rosa <miguelrosa.programacao@gmail.com>
2023-08-06 16:07:42 +0100

    DesenvolvimentoAndroid: AppListaVip - Create Splash Screen (struct)

---
+++ Udemy/Maddo/Desenvolvimento_Android_2023_13_apps_completas/AppListaVip/app/src/main/java/io/github/miguelnunorosa/applistavip/view/SplashActivity.java
@@ -0,0 +1,16 @@
+package io.github.miguelnunorosa.applistavip.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.github.miguelnunorosa.applistavip.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
